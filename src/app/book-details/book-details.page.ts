557e86cb9095794f2f25321205e600cd1aeacf4b
Pedro Jorge <pedro.paulo@gubee.com.br>
2022-01-24 13:29:29 -0300

    Delete book added function of deleting a book in your library

--- src/app/book-details/book-details.page.ts
+++ src/app/book-details/book-details.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {MyBooksService} from '../my-books/my-books.service';
 import {Book} from '../custom-types/Book.model';
+import {AlertController} from '@ionic/angular';
 
 @Component({
   selector: 'app-book-details',
@@ -10,9 +11,41 @@ import {Book} from '../custom-types/Book.model';
 })
 export class BookDetailsPage implements OnInit {
   book: Book;
-  constructor(private activatedRoute: ActivatedRoute, private myBooksService: MyBooksService) {}
+  constructor(private activatedRoute: ActivatedRoute,
+              private myBooksService: MyBooksService,
+              private router: Router,
+              private alertController: AlertController) {}
   ngOnInit() {
-    const bookId= this.activatedRoute.snapshot.paramMap.get('book-id');
-    this.book= this.myBooksService.findBookInList(bookId);
+    this.activatedRoute.paramMap.subscribe(paramMap => {
+      if(!paramMap.has('book-id')){
+        this.router.navigate(['/folder/myBooks']);
+        return;
+      }
+      const bookId= paramMap.get('book-id');
+      this.book= this.myBooksService.findBookInList(bookId);
+    });
+  }
+  deleteBook(){
+    this.alertController.create({
+      header: 'Please confirm your choice',
+      message: 'Do you want to delete the book from your library ? this action is irreversible',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        },
+        {
+          text: 'Delete',
+          handler: () => {
+            this.myBooksService.deleteBook(this.book.id);
+            this.router.navigate(['/folder/myBooks']);
+          }
+        }
+      ]
+    }).then(alertEl => {
+      alertEl.present();
+    });
+  }
+  changeBook(){
   }
 }
